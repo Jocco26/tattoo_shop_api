3f8cc7b950d9269fab25759a65b80146d99d1ba6
unknown <joks026@gmail.com>
2021-07-01 00:52:24 +0800

    addional picture

--- resources/js/components/Homebody.vue
+++ resources/js/components/Homebody.vue
@@ -1,14 +1,33 @@
 <template>
-    <section id="features">
-        
+    <section id="features" v-bind:style="{ backgroundImage: 'bg' }">
                 <div class="DAlogo" id="logo1">
-                    <h1 class="DAlogo"><img id="centerlogo" src="img/DA.png"></a></h1>
+                    <h1 class="DAlogo"><img :src="logo"></h1>
                     <form action="booknow.php" method="get">
                     <button class="DAlogo btn btn-outline-light btn-lg" type="submit">
                     ASK FOR A QUOTE
                     </button>
                     </form>
-                </div>
-        
+                </div>   
     </section>
 </template>
+<script>
+    export default {
+        data(){
+            return{
+                logo: 'https://github.com/Jocco26/tattoo_shop_api/blob/main/src/assets/imgs/DA.png?raw=true',
+                bg:''
+            }
+        }
+    }
+</script>
+<style>
+#features{
+  background-size: cover;
+  background-attachment: fixed;
+  min-height: 500px;
+}
+
+.DAlogo{
+    text-align:center;
+  }
+</style>
