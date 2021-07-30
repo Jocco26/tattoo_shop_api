05cd7f211ab0d27dc5f251b4b6d59a64b44201e1
unknown <joks026@gmail.com>
2021-07-30 18:11:39 +0800

    fixing location

--- resources/js/components/Homebody.vue
+++ resources/js/components/Homebody.vue
@@ -162,7 +162,7 @@
                             </form>
                         </div>
                     </div>
-                    <div class="col-md-6 col-sm-12">
+                    <div id="location" class="col-md-6 col-sm-12">
                         <h6><b>LOCATION</b></h6>
                         <p><iframe id="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2295.0233183703053!2d121.07651653973024!3d14.669910722188467!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397b7ec83c708e9%3A0x17dc05be0cec2d85!2sDon%20Antonio%20Tattoo!5e0!3m2!1sen!2sph!4v1625864769409!5m2!1sen!2sph" style="border:0;" allowfullscreen="" loading="lazy"></iframe></p>
                     </div>
@@ -360,7 +360,9 @@
     margin-top: 20px;
 }
 
-
+#location{
+    padding-top: 50px;
+}
 
 #map{
     width: 300px;
